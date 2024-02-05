2cf9d644f60353207ebfeee453a054c4ca0b116e
ky0ufu <superjar@mail.ru>
2024-02-05 20:19:19 +1000

    fix info

--- network_connection.py
+++ network_connection.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from zeroconf import ServiceBrowser, Zeroconf, ServiceListener, ServiceInfo
 import socket
 import time
+import platform
 
 SERVICE_TYPE = "_https._tcp.local."
 
@@ -12,19 +13,24 @@ def get_ip_address():
 
 def register_service():
     ip_address = get_ip_address()
-    service_name = ip_address
+    service_name = platform.system()
     service_type = SERVICE_TYPE
     service_port = 8080
 
     info = ServiceInfo(
         type_=service_type,
         name="{}.{}".format(service_name, service_type),
+        addresses=[ip_address],
         port=service_port,
+        weight=0,
+        priority=0,
         properties={"info": "Additional information about the device"}
     )
 
     zeroconf = Zeroconf()
     zeroconf.register_service(info, allow_name_change=True)
+    info = zeroconf.get_service_info(SERVICE_TYPE, "{}.{}".format(service_name, service_type))
+    #print(info)
 
     print("Service '{}' registered on port {}".format(service_name, service_port))
 
@@ -38,56 +44,50 @@ class MyListener(ServiceListener):
     def __init__(self):
         self.services = {}
 
+
     def remove_service(self, zeroconf, type, name):
         print(f"Service {name} removed")
         if name in self.services:
             del self.services[name]
         self.show_services()
 
+
     def add_service(self, zeroconf, type_, name):
-        info = zeroconf.get_service_info(type_, name, timeout=5)
-        print('\n', info)
-        self.services[name] = {"port": 80}
+        info = zeroconf.get_service_info(type_, name)
+        print(info)
+
+        ip = socket.inet_ntoa(info.addresses[0])
+        self.services[name] = {"port": info.port, "ip": ip}
+
         print(f"Service {name} added")
         self.show_services()
 
-    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+
+    def update_service(self, zeroconf, type_, name):
         print(f"Service {name} updated")
     
+
     def show_services(self):
         tree.delete(*tree.get_children())
         for name, service in self.services.items():
-            tree.insert("", "end", values=(name, service["port"]))
+            tree.insert("", "end", values=(name.split('._')[0], service["ip"]))
 
 
-print(socket.gethostname())
-zeroconf = register_service()
-listener = MyListener()
 
-
-browser = ServiceBrowser(zeroconf, SERVICE_TYPE, listener)
-
-# Создаем основное окно Tkinter
 root = tk.Tk()
 root.title("Service Browser")
 
-# Создаем и размещаем виджет Treeview для отображения сервисов
-tree = ttk.Treeview(root, columns=("Name", "Address", "Port"), show="headings")
-tree.heading("Name", text="Name")
-tree.heading("Address", text="Address")
-tree.heading("Port", text="Port")
+tree = ttk.Treeview(root, columns=("Platform", "IP"), show="headings")
+tree.heading("Platform", text="Platform")
+tree.heading("IP", text="IP")
 tree.pack(padx=10, pady=10)
 
-# Создаем кнопку "Show" для отображения сервисов
-show_button = tk.Button(root, text="Show", command=listener.show_services())
-show_button.pack(pady=10)
 
-# Регистрируем сервис и создаем объект MyListener
+zeroconf = register_service()
+listener = MyListener()
 
-# Создаем ServiceBrowser для поиска сервисов
+browser = ServiceBrowser(zeroconf, SERVICE_TYPE, listener)
 
-# Запускаем главный цикл Tkinter
 root.mainloop()
 
-# Закрываем zeroconf при выходе из главного цикла
 zeroconf.close()
